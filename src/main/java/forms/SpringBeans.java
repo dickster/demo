2a5a9f28c925236e82de11001cdeb51f02f50ee1
dickster <derek.ipod@gmail.com>
2015-09-23 16:49:23 -0400

    beans

--- src/main/java/forms/SpringBeans.java
+++ src/main/java/forms/SpringBeans.java
@@ -1,22 +1,28 @@
 package forms;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+
 
 @Configuration
+@PropertySource("classpath:/demo/application.properties")
 public class SpringBeans {
 
-    @Bean
+    @Lazy @Bean @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public Toolkit toolkit() {
         return new Toolkit();
     }
 
-    @Bean
+    @Bean @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public Theme theme() {
         return new DefaultTheme();
     }
 
-    @Bean
+    @Lazy @Bean
     public WfFactory WorkflowFactory() {
         return new WfFactory();
     }
---
+++ src/main/java/forms/StateBeans.java
@@ -0,0 +1,18 @@
+package forms;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
+public class StateBeans {
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public WfState dummyState() {
+        return new WfState("production");
+    }
+}
