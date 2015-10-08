b27438ce7d70814c30f9e3609f35d6d0dbc6b03b
dickster <derek.ipod@gmail.com>
2015-10-08 11:27:11 -0400

    asdfadsf

---
+++ src/main/java/forms/TestData.java
@@ -0,0 +1,11 @@
+package forms;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: derek.dick
+ * Date: 9/28/15
+ * Time: 9:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestData {
+}
---
+++ src/main/java/forms/WfWicketUtil.java
@@ -0,0 +1,25 @@
+package forms;
+
+
+import org.apache.wicket.Component;
+
+import javax.annotation.Nonnull;
+
+// wicket/workflow integration util.
+public class WfWicketUtil {
+
+    public static void post(@Nonnull Component component, @Nonnull WfEvent event) {
+        Workflow workflow = getWorkflow(component);
+        workflow.post(event);
+    }
+
+    private static @Nonnull Workflow getWorkflow(@Nonnull Component component) {
+        WorkflowPage parent = component.findParent(WorkflowPage.class);
+        if (parent==null) {
+            throw new IllegalStateException("uh oh, can't find workflow....this is not valid state of affairs!!");
+        }
+        return parent.getWorkflow();
+    }
+
+}
+
