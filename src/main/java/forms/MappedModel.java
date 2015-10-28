03db8875718ed81b32e973104831b58a399e6714
dickster <derek.ipod@gmail.com>
2015-10-28 12:25:27 -0400

    oct 22

--- src/main/java/forms/MappedModel.java
+++ src/main/java/forms/MappedModel.java
@@ -1,11 +1,16 @@
 package forms;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.apache.wicket.Component;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.IWrapModel;
 
+import java.util.List;
 import java.util.Map;
 
 public class MappedModel extends CompoundPropertyModel<Map<String, Object>> {
@@ -61,5 +66,57 @@ public class MappedModel extends CompoundPropertyModel<Map<String, Object>> {
     public String toString() {
         String values = Joiner.on(", ").withKeyValueSeparator("->").join(getObject());
         return "MappedModel:{" + values + "}";
+    }
+
+    public JsonElement asJson() {
+        JsonResult result = new JsonResult();
+        Map<String, Object> map = getObject();
+
+        for (String key:map.keySet()) {
+            Object value = map.get(key);
+            result.add(key, value);
+        }
+        return result.getJson();
+    }
+
+
+
+    class JsonResult {
+        private JsonObject json = new JsonObject();
+        private String property;
+        private List<String> parent;
+
+        JsonResult() {
+        }
+        public void add(String key, Object value) {
+            List<String> properties = Lists.newArrayList(key.split("."));
+            if (properties.size()>1) {
+                parent = properties.subList(0,properties.size()-1);
+                property = properties.get(properties.size()-1);
+            }
+            else {
+                parent = Lists.newArrayList();
+                property = properties.get(0);
+            }
+            add(parent, property, value);
+        }
+
+        private void add(List<String> parent, String property, Object value) {
+            //getParent().addProperty(property, new Gson().toJsonTree(value));
+        }
+
+        private JsonObject getParent() {
+            //blah blah blah..
+            return new JsonObject();
+        }
+
+
+        private JsonElement jsonValue(Object value) {
+            return new Gson().toJsonTree(value);
+        }
+
+        public JsonObject getJson() {
+            return json;
+        }
     }
 }
